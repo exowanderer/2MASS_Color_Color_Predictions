65d2e238c5f4f61e76e8e19a63786297d2e6e4be
Jonathan Fraine <exowanderer@users.noreply.github.com>
2017-07-06 17:16:07 -0400

    Found more functional PySynPhot Usage
    @JarronL provided me with a proper usage of PySynPhot to generate synthetic photometric observations of JHK bands for stars with Vmag=10

--- generate_JH_HK_colors.py
+++ generate_JH_HK_colors.py
@@ -1,33 +1,31 @@
+import pynrc
+
 from pylab import *;ion()
 import pysynphot as S
 
 from scipy.interpolate import CubicSpline
 
-# Load the Filters
-JBandFilterWave, JBandFilterThru = np.loadtxt('2MASS-2MASS.J.dat').T # load the J filter
-HBandFilterWave, HBandFilterThru = np.loadtxt('2MASS-2MASS.H.dat').T # load the H filter
-KBandFilterWave, KBandFilterThru = np.loadtxt('2MASS-2MASS.Ks.dat').T # load the K filter
-
-# Interpolate the Filters onto the Stellar Model Wavelength Grid
-tempModel = S.Icat('phoenix', 5500, 0.0, 4.5) # we are going to use this to setup the filters in the SYNPHOT framework
-
-# For some reason pysnphot stores the Vega spectrum with different number of flux points
-JWaveVRange= (S.Vega.wave >= JBandFilterWave.min()) * (S.Vega.wave <= JBandFilterWave.max()) # isolate model region in JBand
-HWaveVRange= (S.Vega.wave >= HBandFilterWave.min()) * (S.Vega.wave <= HBandFilterWave.max()) # isolate model region in HBand
-KWaveVRange= (S.Vega.wave >= KBandFilterWave.min()) * (S.Vega.wave <= KBandFilterWave.max()) # isolate model region in KBand
-
-VegaJFlux = np.trapz(S.Vega.flux[JWaveVRange] * JBandFilterSpline(S.Vega.wave[JWaveVRange]))
-VegaHFlux = np.trapz(S.Vega.flux[HWaveVRange] * HBandFilterSpline(S.Vega.wave[HWaveVRange]))
-VegaKFlux = np.trapz(S.Vega.flux[KWaveVRange] * KBandFilterSpline(S.Vega.wave[KWaveVRange]))
-
-# Now to compare the JHK filters to the PHOENIX models
-JWaveRange= (tempModel.wave >= JBandFilterWave.min()) * (tempModel.wave <= JBandFilterWave.max()) # isolate model region in JBand
-HWaveRange= (tempModel.wave >= HBandFilterWave.min()) * (tempModel.wave <= HBandFilterWave.max()) # isolate model region in HBand
-KWaveRange= (tempModel.wave >= KBandFilterWave.min()) * (tempModel.wave <= KBandFilterWave.max()) # isolate model region in KBand
-
-JBandFilterSpline = CubicSpline(JBandFilterWave, JBandFilterThru)
-HBandFilterSpline = CubicSpline(HBandFilterWave, HBandFilterThru)
-KBandFilterSpline = CubicSpline(KBandFilterWave, KBandFilterThru)
+def get_magnitudes(stellarType = 'G2V'):
+    # Load the Filters
+    bp_j = S.ObsBandpass('j')
+    bp_h = S.ObsBandpass('h')
+    bp_k = S.ObsBandpass('k')
+    bp_v = S.ObsBandpass('johnson,v')
+    
+    # G2V spectrum normalized to K=10 mags (default Phoenix models)
+    sp = pynrc.stellar_spectrum(stellarType, 10, 'vegamag', bp_v)
+    
+    # Observe in J, H, and K
+    obs_j = S.Observation(sp, bp_j, binset=sp.wave)
+    obs_h = S.Observation(sp, bp_h, binset=sp.wave)
+    obs_k = S.Observation(sp, bp_k, binset=sp.wave)
+    
+    # Magnitudes in each filter
+    mag_j = obs_j.effstim('vegamag')
+    mag_h = obs_h.effstim('vegamag')
+    mag_k = obs_k.effstim('vegamag')
+    
+    return mag_j, mag_h, mag_k
 
 # Loop over the 30 models
 nModels   = 30
@@ -42,9 +40,7 @@ assert(len(teff_list) == nModels)
 
 for kt, teff in enumerate(teff_list):
     modelTeff = S.Icat('phoenix', teff, 0.0, 4.5) # load the model
-    Jmags[kt] = -2.5*np.log10(np.trapz(modelTeff.flux[JWaveRange] * JBandFilterSpline(tempModel.wave[JWaveRange]) / VegaJFlux)) # integrate the flux in the J bandpass
-    Hmags[kt] = -2.5*np.log10(np.trapz(modelTeff.flux[HWaveRange] * HBandFilterSpline(tempModel.wave[HWaveRange]) / VegaHFlux)) # integrate the flux in the H bandpass
-    Kmags[kt] = -2.5*np.log10(np.trapz(modelTeff.flux[KWaveRange] * KBandFilterSpline(tempModel.wave[KWaveRange]) / VegaKFlux)) # integrate the flux in the K bandpass
+    Jmags[kt], Hmags[kt], Kmags[kt] = get_magnitudes(stellarType = 'G2V')
 
 jhmod = Jmags - Hmags
 hkmod = Hmags - Kmags
